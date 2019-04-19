1383c05f73812dd1aacf5b5dc3f44e0215eb6678
nes-elliott <nspry1997@gmail.com>
2019-04-19 15:52:54 -0400

    complete function runQuery, tested

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,6 +16,41 @@ function runQuery(numArticles, queryURL) {
     console.log("-----------------")
     console.log(NYTData)
     console.log("-----------------")
+
+    for (var i = 0; i < numArticles; i++) {
+      articleCounter++
+      var wellSection = $("<section>")
+      wellSection.addClass("well")
+      wellSection.attr("id", "article-well-" + articleCounter)
+      $("#well-section").append(wellSection)
+
+      if (NYTData.response.docs[i].headline !== "null") {
+        $("#article-well-" + articleCounter)
+          .append(
+            "<h3 class='articleHeadline'><span class='label label-primary'>" + 
+            articleCounter + "</span><strong> " + 
+            NYTData.response.docs[i].headline.main + "</strong></h3>"
+          )
+        console.log(NYTData.response.docs[i].headline.main)
+      }
+      
+      if (NYTData.response.docs[i].byline && NYTData.response.docs[i].byline.original) {
+        $("#article-well-" + articleCounter)
+          .append("<h5>" + NYTData.response.docs[i].byline.original + "</h5>")
+        console.log(NYTData.response.docs[i].byline.original)
+      }
+
+      $("#articleWell-" + articleCounter)
+        .append("<h5>Section: " + NYTData.response.docs[i].section_name + "</h5>")
+        .append("<h5>" + NYTData.response.docs[i].pub_date + "</h5>")
+        .append(
+          "<a href='" + NYTData.response.docs[i].web_url + "'>" + 
+          NYTData.response.docs[i].web_url + "</a>"
+        )
+      console.log(NYTData.response.docs[i].pub_date)
+      console.log(NYTData.response.docs[i].section_name)
+      console.log(NYTData.response.docs[i].web_url)
+    }
   })
 }
 runQuery(1, queryURLBase + "dogs")
