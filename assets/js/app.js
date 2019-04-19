1bf655393f3440bf178904ae6b98b187d30defbb
nes-elliott <nspry1997@gmail.com>
2019-04-19 15:15:40 -0400

    add jquery 3.4.0 due for ajax, tested ajax call, success

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,21 @@
-// authkey
-const [a,b] = ["one", "two"]
-console.log (a)
+var authKey = "OQNu1PWtltUyPArPJ75ACyJpsYiqNeN8" // register a key
+var searchTerm = ""
+var numResults = 0
+var startYear = 0
+var endYear = 0
+var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey + "&q="
+var articleCounter = 0
+
+function runQuery(numArticles, queryURL) {
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(NYTData) {
+    console.log("-----------------")
+    console.log("URL: " + queryURL)
+    console.log("-----------------")
+    console.log(NYTData)
+    console.log("-----------------")
+  })
+}
+runQuery(1, queryURLBase + "dogs")
